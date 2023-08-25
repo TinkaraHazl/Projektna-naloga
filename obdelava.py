3154bd0abab50b22cb47e080150c8fc4cf6c2aa0
Tinkara Hazl <th52887@student.uni-lj.si>
2023-08-25 12:56:49 +0200

    dodajanje vozrcev

--- obdelava.py
+++ obdelava.py
@@ -20,9 +20,9 @@ def get_dict_from_ad_block(block):
     """Funkcija iz niza za posamezen oglasni blok izlušči podatke o imenu, ceni
     in opisu ter vrne slovar, ki vsebuje ustrezne podatke."""
     vzorec_st = r'< td align = "center" class = "fooinfo" >  # (\d{4}) </td>'
-    vzorec_pokemon = r'< td align = "center" class = "fooinfo" > <a href = "/pokemon/\w+" >(\w+)</a ></td >'
-    vzorec_ability = 
-    vzorec_type = 
+    vzorec_pokemon = r'< td align = "center" class = "fooinfo" > <a href = "/pokemon/\w+?" >(\w+?\s?\w+?)</a ></td >'
+    vzorec_ability = r'<a href = "/abilitydex/\w+?\s?\w+?.shtml" > (\w+?\s?\w+?) < /a >'
+    vzorec_type = r'<a href = "/pokemon/type/(\w+?)" > <img src = "/pokedex-bw/type/\w+?.gif" border = "0" / > < /a >'
     vzorec_HP = r'<td align = "center" class = "fooinfo" > (\d{2,3}) </td>'
     vzorec_attack = r'<td align = "center" class = "fooinfo" > \d{2,3} </td> < td align = "center" class = "fooinfo" > (\d{2, 3}) < /td >'
     vzorec_defense = r'<td align = "center" class = "fooinfo" > \d{2,3} </td> <td align = "center" class = "fooinfo" > \d{2,3} </td> <td align = "center" class = "fooinfo" > (\d{2,3}) </td>'
@@ -32,8 +32,8 @@ def get_dict_from_ad_block(block):
     try:
         stevilo = re.search(vzorec_st, block).group(1)
         pokemon = re.search(vzorec_pokemon, block).group(1)
-        type = re.search(vzorec_type, block).group(1)
-        ability = re.search(vzorec_ability, block).group(1)
+        type = re.findall(vzorec_type, block)
+        ability = re.findall(vzorec_ability, block)
         HP =  re.search(vzorec_HP, block).group(1)
         attack = re.search(vzorec_attack, block).group(1)
         defense = re.search(vzorec_defense, block).group(1)
@@ -46,4 +46,14 @@ def get_dict_from_ad_block(block):
     return {"stevilo": stevilo, "pokemon": pokemon, "type": type, "ability": ability, 'HP': HP, 'attack': attack, 
             'defense': defense, 'special attack': sp_attack, 'special defence': sp_defence, 'speed' : speed}
 
+
+def ads_from_file(filename, directory):
+    """Funkcija prebere podatke v datoteki "directory"/"filename" in jih
+    pretvori (razčleni) v pripadajoč seznam slovarjev za vsak oglas posebej."""
+    vsebina = read_file_to_string(directory, filename)
+    oglasi = page_to_ads(vsebina)
+    slovarji = []
+    for oglas in oglasi:
+        slovarji.append(get_dict_from_ad_block(oglas))
+    return slovarji
 
